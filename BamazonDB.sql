fc6b8e69c698119e9088533646c0b83f648df525
bradham <bradham@users.noreply.github.com>
2019-10-15 19:18:16 -0500

    Working code to present list of data

---
+++ BamazonDB.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price DECIMAL(10,2) NULL,                           -- cost to customer
+  stock_quantity INT NULL,                                -- how much of the product is available in stores
+  PRIMARY KEY (item_id)
+);
+
+USE bamazon;
+SELECT * from products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,35 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+//Initiate MySQL
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "brad",
+
+    // Your password
+    password: "123456",
+    database: "bamazon"
+});
+
+// Connection to database
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    //Start Bamazon by showing the products
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].product_name);
+        }
+    });
+
+    connection.end();
+}
